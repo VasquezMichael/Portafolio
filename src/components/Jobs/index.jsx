246a596a23df8e3e656f7e03604ea7055a15b9d7
VasquezMichael <vasquez.michael.steven@gmail.com>
2023-08-30 19:46:29 -0300

    Damos estilos generales a la seccion de proyectos

--- src/components/Jobs/index.jsx
+++ src/components/Jobs/index.jsx
@@ -5,7 +5,7 @@ import web3 from "../../../public/web5.png";
 import Image from "next/image";
 const Jobs = () => {
   return (
-    <div>
+    <div className="mb-10">
       <h3 className="text-3xl py-1">Portafolio</h3>
       <p className="text-md py-2  leading-8 text-gray-800">
         Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
@@ -14,15 +14,18 @@ const Jobs = () => {
         <span className="text-purple-600">repudiandae</span> odio ad placeat
         ipsam commodi delectus dolor.
       </p>
-      <div className="flex flex-col gap-10 py-10 md:flex-row md:flex-wrap">
-        <div className="">
-          <Image src={web1} />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10">
+        <div className="  h-full">
+          <Image src={web1} className="h-full" />
         </div>
         <div className="">
-          <Image src={web2} />
+          <Image src={web2} className="h-full" />
         </div>
-        <div className="">
-          <Image src={web3} />
+        <div className=" ">
+          <Image src={web3} className="h-full" />
+        </div>
+        <div className=" ">
+          <Image src={web1} className="h-full" />
         </div>
       </div>
     </div>

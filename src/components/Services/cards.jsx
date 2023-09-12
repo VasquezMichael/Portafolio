6aeed52d8aee0d13f37769e68dbf462eab77fe3b
VasquezMichael <vasquez.michael.steven@gmail.com>
2023-09-12 20:11:52 -0300

    Terminamos de estilizar la seccion de skills

--- src/components/Services/cards.jsx
+++ src/components/Services/cards.jsx
@@ -4,6 +4,8 @@ import html from "../../../public/html-icon.png";
 import github from "../../../public/github-icon.png";
 import js from "../../../public/javascript-icon.png";
 import react from "../../../public/react-js-icon.png";
+import node from "../../../public/nodejs.png";
+import mysql from "../../../public/mysql.png";
 import git from "../../../public/git-icon.png";
 import css from "../../../public/css-icon.png";
 const cards = () => {
@@ -12,22 +14,52 @@ const cards = () => {
       id="section-cards"
       className="grid grid-cols-2 md:grid-cols-4 gap-5 my-10"
     >
-      <div id="card" className=" shadow-xl rounded-xl w-28 h-28 p-4 mx-auto ">
+      <div
+        id="card"
+        className=" shadow-xl rounded-xl w-28 h-28 p-4 mx-auto dark:bg-gray-100  hover:scale-110 duration-300"
+      >
         <Image src={js} className="w-full h-full object-cover" />
       </div>
-      <div id="card" className=" shadow-xl rounded-xl w-28 h-28 p-4 mx-auto">
+      <div
+        id="card"
+        className=" shadow-xl rounded-xl w-28 h-28 p-4 mx-auto dark:bg-gray-100 hover:scale-110 duration-300"
+      >
         <Image src={react} className="w-full h-full object-contain" />
       </div>
-      <div id="card" className=" shadow-xl rounded-xl w-28 h-28 p-4 mx-auto">
+      <div
+        id="card"
+        className=" shadow-xl rounded-xl w-28 h-28 p-4 mx-auto dark:bg-gray-100 hover:scale-110 duration-300"
+      >
         <Image src={github} className="w-full h-full object-contain" />
       </div>
-      <div id="card" className=" shadow-xl rounded-xl w-28 h-28 p-4  mx-auto">
+      <div
+        id="card"
+        className=" shadow-xl rounded-xl w-28 h-28 p-4  mx-auto dark:bg-gray-100 hover:scale-110 duration-300"
+      >
         <Image src={git} className="w-full h-full object-contain" />
       </div>
-      <div id="card" className=" shadow-xl rounded-xl w-28 h-28 p-4  mx-auto">
+      <div
+        id="card"
+        className=" shadow-xl rounded-xl w-28 h-28 p-4  mx-auto dark:bg-gray-100 hover:scale-110 duration-300"
+      >
+        <Image src={mysql} className="w-full h-full object-contain" />
+      </div>
+      <div
+        id="card"
+        className=" shadow-xl rounded-xl w-28 h-28 p-4  mx-auto dark:bg-gray-100 hover:scale-110 duration-300"
+      >
+        <Image src={node} className="w-full h-full object-contain" />
+      </div>
+      <div
+        id="card"
+        className=" shadow-xl rounded-xl w-28 h-28 p-4  mx-auto dark:bg-gray-100 hover:scale-110 duration-300"
+      >
         <Image src={html} className="w-full h-full object-contain" />
       </div>
-      <div id="card" className=" shadow-xl rounded-xl w-28 h-28 p-4  mx-auto">
+      <div
+        id="card"
+        className=" shadow-xl rounded-xl w-28 h-28 p-4  mx-auto dark:bg-gray-100 hover:scale-110 duration-300"
+      >
         <Image src={css} className="w-full h-full object-contain" />
       </div>
     </section>
--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -4,14 +4,7 @@ const Services = () => {
   return (
     <section className="">
       <div className="Habilidades">
-        <h3 className="text-3xl py-1 dark:text-white">Skills</h3>
-        <p className="text-md py-2  leading-8 text-gray-800 dark:text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
-          <span className="text-purple-600">Quibusdam</span> ab, quae, debitis
-          vel nam vitae quidem expedita{" "}
-          <span className="text-purple-600 ">repudiandae</span> odio ad placeat
-          ipsam commodi delectus dolor.
-        </p>
+        <h3 className="text-4xl py-1 dark:text-white">Skills</h3>
         <p className="text-md py-2  leading-8 text-gray-800 dark:text-gray-300">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
           assumenda voluptatibus reprehenderit, dicta debitis.
